86ca7a1c62e5fc2cf31e21e4b81d61655599b13d
Dmytro <bestro201@gmail.com>
2024-03-11 11:14:37 +0300

    coment

---
+++ src/StringUtil.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class StringUtil {
+    public static List<String> getUniqueSortedWords(String input) {
+
+        String normaInput = input.replaceAll("[,.!\"«»]", "").toLowerCase();
+
+        List<String> words = Arrays.asList(normaInput.split("\\s+"));
+        Set<String> uniqueWordsSet = new HashSet<>(words);
+        return uniqueWordsSet.stream()
+                .sorted(Comparator.comparingInt(String::length).thenComparing(Comparator.naturalOrder()))
+                .collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) {
+        String testString = "Тестовая строка для удаления слов, которые повторяются. \"строка\" для удаления!";
+        System.out.println(StringUtil.getUniqueSortedWords(testString));
+    }
+}
+
